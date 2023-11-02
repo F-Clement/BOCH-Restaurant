b1637ddd759b5d5e59ae8c98c1f30273bbf1b528
F-Clement <>
2023-11-02 16:03:17 +0000

    Modify allowed host url for heroku

--- bochrestaurant/settings.py
+++ bochrestaurant/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://bochrestaurant-09e413a21f67.herokuapp.com/',
+ALLOWED_HOSTS = ['bochrestaurant-09e413a21f67.herokuapp.com',
                  '8000-f-clement-boch-restauran-dn9t9qoeq5.us2.codeanyapp.com']
 
 
