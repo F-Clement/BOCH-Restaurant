cf2a54243866e8f0118f016bf6c20fff970656e7
F-Clement <>
2023-11-03 22:30:00 +0000

    create and migrate model for reservations

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+# Reservation model for users can make reservations
+class Reservation(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=70)
+    phone = models.IntegerField()
+    number_of_people = models.IntegerField()
+    date = models.DateField()
+    time = models.TimeField()
+
+    def __str__(self):
+        return self.name
