d72cbc6581aa4a51d2c1797b8b1c735369bca562
F-Clement <>
2023-11-03 23:02:42 +0000

    Register Reservation model

--- restaurant/admin.py
+++ restaurant/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Reservation
 
-# Register your models here.
+# Register Reservation model
+
+# admin.site.register(Reservation)
+
+
+@admin.register(Reservation)
+class ReservationAdmin(admin.ModelAdmin):
+    list_display = ('name', 'phone', 'date', 'time')
